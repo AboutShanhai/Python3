9d9497674e10cf203b0d201391d80f2af12dad59
shanhai <shanhai@gmail.com>
2021-05-01 20:51:13 +0700

    IP字符串的提取有效IP

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ def print_hi(name):
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print_hi('PyCharm')
+    import re
+
+    pattern = re.compile(r'((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}')
+    string_ip = u'当前 IP：35.241.80.59  来自于：中国 香港   cloud.google.com'
+    print(string_ip)
+    result_pattern = pattern.search(string_ip)
+    print(pattern.search(string_ip))
+
+    result = re.findall(
+        r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", string_ip)
+    print(result)
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
