aa35d64867082bbf5f35a33ad9e2abc906e7d595
shanhai <shanhai@gmail.com>
2021-06-03 21:43:43 +0700

    1.HelloWorld 2.目录单个工程分开

---
+++ IpFilter/IpFilter.py
@@ -0,0 +1,15 @@
+#!user/bin/python
+# -*- coding: UTF-8 -*-
+
+import re
+
+if __name__ == '__main__':
+    pattern = re.compile(r'((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}')
+    string_ip = u'当前 IP：35.241.80.59  来自于：中国 香港   cloud.google.com'
+    print(string_ip)
+    result_pattern = pattern.search(string_ip)
+    print(pattern.search(string_ip))
+
+    result = re.findall(
+        r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b", string_ip)
+    print(result)
