8877e154530b4680b7b33fc43cd62c812a302841
shanhai <shanhai@gmail.com>
2021-06-04 22:59:10 +0700

    斐波纳契数列

--- FibonacciSeries/FibonacciSeries.py
+++ FibonacciSeries/FibonacciSeries.py
@@ -1,10 +1,19 @@
 #!user/bin/python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
+# @Time : 2021/6/4 22:57
+# @Author : shanhai
+# @File : FibonacciSeries.py
+# @Software: PyCharm
 
-if __name__ == '__main__':
+
+def FibonacciFunction(num=5):
     print('斐波纳契数列', end=":")
     a, b, c = 0, 1, 1
     while c <= 10:
         print(b, end=',')
         a, b = b, a + b
         c = c + 1
+
+
+if __name__ == '__main__':
+    FibonacciFunction(10)
