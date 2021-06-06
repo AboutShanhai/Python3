acfd4c529473d86760bfad4a61a8a9c4d8bd6a4a
shanhai <shanhai@gmail.com>
2021-06-06 21:40:13 +0700

    用列表实现 耶稣的叛徒

---
+++ JesusTraitor/JesusTraitor.py
@@ -0,0 +1,31 @@
+#!usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/6/6 20:22
+# @Author : shanhai
+# @File : JesusTraitor.py
+# @Software: PyCharm
+
+
+def JesusTraitor():
+    people = list(range(1, 11))
+    print(people)
+    count = 1
+    index = 0
+    flag = 0
+    while count < len(people):
+        index = index % len(people)
+        if people[index] != 0:
+            flag = flag + 1
+
+        if 3 == flag:
+            people[index] = 0
+            flag = 0
+            count = count + 1
+
+        index = index + 1
+
+    print(people)
+
+
+if __name__ == '__main__':
+    JesusTraitor()
